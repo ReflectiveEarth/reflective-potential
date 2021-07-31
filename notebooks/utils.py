968f06e87e00a6a2f5fa5e0f9064842f820dda46
Brian Smoliak <brian@reflectiveearth.org>
2021-07-31 18:45:25 +0000

    Add python utils

---
+++ notebooks/utils.py
@@ -0,0 +1,8 @@
+from os import environ
+from warnings import warn
+
+
+def check_environment(name):
+    """Check the current conda environment and warn if other than expected."""    
+    if environ["CONDA_DEFAULT_ENV"] != name:
+        warn(f"conda environment: {name} not activated. Some dependencies may not be installed.")
