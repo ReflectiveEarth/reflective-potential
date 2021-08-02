b364b9dd84cc350a6e4da7063801809046b4d74d
Brian Smoliak <brian@reflectiveearth.org>
2021-08-01 20:08:54 -0500

    Update notebooks

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -5,4 +5,5 @@ from warnings import warn
 def check_environment(name):
     """Check the current conda environment and warn if other than expected."""    
     if environ["CONDA_DEFAULT_ENV"] != name:
-        warn(f"conda environment: {name} not activated. Some dependencies may not be installed.")
+        logging.warning(f"conda environment: {name} not activated." 
+                         "Some dependencies may not be installed.")
